5a4051ba5c3311e3e70b90e63d34c694bc232050
Oliver Beach <17b23772@marling.school>
2020-10-07 08:59:53 +0000

    Finished Code, Added Random

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 guesses = 1
-number = 4
+from random import randint
+number = randint(1,10)
 print("Guess a number between 1 and 10")
+print("You Have 3 Guesses!")
 guess = int(input())
 while guess != number:
   print("Incorrect")
   guesses = guesses + 1
   print("Guess a number between 1 and 10")
   guess = int(input())
+  if guess == number:
+    print ("Correct")
   if guesses == 3:
     print ("Game Over. You Used Your 3 Guesses.")
     break
 else:
-  print("Correct")
+  print("Correct!")
   print("These Are Your Ammount Of Guesses: ")
   print (guesses)
